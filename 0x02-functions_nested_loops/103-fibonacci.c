e392718ecee3ab13c62d94384594a9c707d93136
Salmaah369 <salmaah78@gmail.com>
2023-04-26 18:17:05 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - Prints the sum of Even Fibonacci numbers 
- * less than 4000000
- * Return: Nothing!
- */
+* main - Prints the sum of Even Fibonacci numbers 
+* less than 4000000
+* Return: Nothing!
+*/
 int main(void)
 {
 int i = 0;
