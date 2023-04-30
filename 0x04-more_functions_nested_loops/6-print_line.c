530ee5046e82ff8ed33b7ba04cb1a22e43102746
Salmaah369 <salmaah78@gmail.com>
2023-04-30 08:32:37 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - prints staight line n times.
+* @n: times staight line is printed.
+* Return: no retun.
+*/
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
