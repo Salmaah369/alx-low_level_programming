6246198ac31ad759dac0cd3b49ac25de9ebf7ffc
Salmaah369 <salmaah78@gmail.com>
2023-05-02 20:38:38 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,12 @@
 /**
 * _puts - Prints a string
 * @str: The string to print
-* Return: void
 */
 void _puts(char *str)
 {
-for (; *str != '\0'; str++)
+while (*str != '\0')
 {
-_putchar(*str);
+_putchar(*str++);
 }
 _putchar('\n');
 }
